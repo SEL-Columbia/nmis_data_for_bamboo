887afb4d8494660f1365aa6a88fbde01d1181055
Myf Ma <myf@worldtalks.org>
2013-06-04 15:52:47 -0400

    added json file for presistency

--- push_to_bamboo.py
+++ push_to_bamboo.py
@@ -1,4 +1,10 @@
 from pybamboo.connection import Connection
 from pybamboo.dataset import Dataset
 import os
+import simplejson as json
 
+with open('bamboo_hash.json','r') as f:
+  hash_json = json.loads(f.read())
+
+
+#Education_Facilities.csv  Education_LGA.csv  Health_Facilities.csv  Health_LGA.csv  Makefile  Water_Facilities.csv  Water_LGA.csv
